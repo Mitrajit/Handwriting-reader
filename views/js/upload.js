3af0096fe11482d9c537c1fb0571b1c67cd71b33
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-11-17 18:24:16 +0530

    Upload pg changed not redirect & check OCR status

--- views/js/upload.js
+++ views/js/upload.js
@@ -1,17 +1,51 @@
-function formSubmit(event) {
-    const url = "/upload";
-    fetch(url, {
-        method: 'POST',
-        body: new FormData(event.target)
-    }).then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        console.log(json);
-    }).catch(function (ex) {
-        console.log('parsing failed', ex);
-    });
+async function formSubmit(event) {
     event.preventDefault();
+    try {
+        const url = "/upload";
+        json = await (await fetch(url, {
+            method: 'POST',
+            body: new FormData(event.target)
+        })).json();
+        console.log(json);
+        ocrcheckup();
+        checkupinterval = setInterval(ocrcheckup, 1000);
+    } catch (ex) {
+        console.log('parsing failed', ex);
+    }
 }
+var checkupinterval, json;
+async function ocrcheckup() {
+    try {
+        if(!json)
+        return;
+        const responsejson = await (await fetch("/ocrcheck", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                operationId: json.operationId,
+                filename: json.filename
+            })
+        })).json();
 
+        console.log(responsejson);
+        if (responsejson.status === "succeeded") {
+            document.querySelector("#status").innerHTML = "<h1>OCR has succeeded</h1>";
+            clearInterval(checkupinterval);
+            window.location.href = "/result/" + responsejson.operationId;
+        }
+        else if (responsejson.status === "failed") {
+            clearInterval(checkupinterval);
+            alert("OCR failed");
+        }
+        else if (responsejson.status === "running") {
+            document.querySelector("#status").innerHTML = "<h1>OCR is running</h1>";
+        }
+    } catch (ex) {
+        console.log('parsing failed', ex);
+        clearInterval(checkupinterval);
+    }
+}
 // Custom function to upload file wihtout redirection
 document.getElementById("uploadform").addEventListener("submit", formSubmit);
