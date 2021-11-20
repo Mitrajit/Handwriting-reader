24927db1165bd32d30fb8e75f1cea9101357079e
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-11-20 16:34:50 +0530

    Results view added

---
+++ views/js/result.js
@@ -0,0 +1,18 @@
+const ocrtext = document.querySelector('#ocrtext');
+
+fetch(window.location.pathname + '/text', {
+    method: 'GET'
+}).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    for (const textRecResult of data.content) {
+        for (const line of textRecResult.lines) {
+            ocrtext.innerHTML += line.text + "\n";
+        }
+    }
+}).catch(function (err) {
+    console.log(err);
+});
+
+document.querySelector("audio source").src = window.location.pathname + "/audio";
+document.querySelector("audio").load();
