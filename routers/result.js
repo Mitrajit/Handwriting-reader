606f61ae70bce266c59397e336695ee9c599d48f
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-12-02 20:32:53 +0530

    404 page made and shows on wrong id

--- routers/result.js
+++ routers/result.js
@@ -4,12 +4,11 @@ const {OCRcheck} = require("../modules/OCR");
 const fs = require("fs");
 
 router.get("/:id", (req, res) => {
-    // const file = __dirname+`/../audio`;
-    // fs.access(file, constants.F_OK, (err) => {
-    //   console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
-    // });
-
-  res.sendFile("views/result.html", {root: __dirname+"/../"});
+  const file = __dirname + "/../audios/" + req.params.id + ".wav";
+  if (fs.existsSync(file))
+    res.sendFile("views/result.html", { root: __dirname + "/../" });
+  else
+    res.status(404).sendFile("views/404.html", { root: __dirname + "/../" });
 });
 
 router.get("/:id/text",async (req,res)=>{
