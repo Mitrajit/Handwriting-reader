c988ef5a8337aaca8d43c91fcced25e77a6feb1d
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-12-04 16:28:12 +0530

    404 page for all invalid requests

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.use('/audio', express.static(path.join(__dirname, '/audios')));
 app.use("/", upload);
 app.use("/result", result);
 
+app.get('*', function(req, res){
+    res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  });
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 });
