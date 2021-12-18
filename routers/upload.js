ad853a8dc78c234903430092152936f8d8b123f2
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-12-18 21:18:23 +0530

    formidable used instead of multer & cleanups [Backend]

--- routers/upload.js
+++ routers/upload.js
@@ -1,56 +1,67 @@
 const express = require("express");
-const multer = require('multer');
-const {OCRupload, OCRcheck} = require("../modules/OCR");
+const { OCRupload } = require("../modules/OCR");
 const fs = require("fs");
+const formidable = require('formidable');
+const path = require("path");
 // Create uploads directory if it doesn't exist
 fs.existsSync("./uploads") || fs.mkdirSync("./uploads");
 
-// Defining multer instance for destination and file name
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, uniqueSuffix+'-'+file.originalname)
-    }
-  });
-// Creating the multer instance
-const upload = multer({ storage: storage });
 const router = express.Router();
 // Main upload page
 router.get("/", (req, res) => {
-    // open the HTML file
-    res.sendFile("views/upload.html", {root: __dirname+"/../"});
+  // open the HTML file
+  res.sendFile("views/upload.html", { root: __dirname + "/../" });
 });
 
-router.post("/upload", upload.single('handwriting'), async (req, res) => {
-    try{
-    if(req.file) {
-        // Call the OCR service
-        const operationId = await OCRupload(__dirname+"/../"+req.file.path);
-        res.status(200).json(Object.assign(req.file,{operationId: operationId}));
-    }
-    else throw 'File not uploaded';
+router.post("/upload", (req, res) => {
+  try {
+    const form = new formidable.IncomingForm();
+    const uploadFolder = path.join(__dirname, "../uploads");
+
+    // Basic Configuration
+    form.multiples = true;
+    form.maxFileSize = 5 * 1024 * 1024; // 5MB
+    form.uploadDir = uploadFolder;
+    form.parse(req, async function (err, fields, files) {
+      if (err) {
+        console.log("Error parsing the files");
+        return res.status(400).json({
+          status: "Fail",
+          message: "There was an error parsing the files",
+          error: err,
+        });
+      }
+
+      var oldPath = files.handwriting.filepath;
+      var newPath = path.join(uploadFolder, Date.now() + '-' + Math.round(Math.random() * 1E9) + files.handwriting.originalFilename);
+      var rawData = await fs.readFileSync(oldPath);
+      // Delete temp file
+      fs.unlinkSync(oldPath);
+      fs.writeFile(newPath, rawData, async function (err) {
+        if (err) console.log(err);
+        const operationId = await OCRupload(newPath);
+        res.status(200).json({ operationId: operationId });
+      });
+    });
   }
-  catch(err){
+  catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.post("/ocrcheck",async (req,res)=>{
-  try{
+router.post("/ocrcheck", async (req, res) => {
+  try {
     // Check if the operation is done
     const { operationId } = req.body;
-    if (fs.existsSync(__dirname + "/../audios/" + operationId + "/audio.mp3")) 
+    if (fs.existsSync(__dirname + "/../audios/" + operationId + "/audio.mp3"))
       return res.status(200).json({ status: "succeeded" });
     else
       return res.status(200).json({ status: "running" });
   }
-  catch(err){
+  catch (err) {
     console.log(err);
-    res.status(500).json({status: 'failed', error: err});
+    res.status(500).json({ status: 'failed', error: err });
   }
 });
 module.exports = router;
